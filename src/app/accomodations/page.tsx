ff790794bff5eca2b1ae3408138453026692b747
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-09-23 10:52:49 -0300

    Update: Estudo realizado sobre componentes com filhos (children) e navegação entre rotas

---
+++ src/app/accomodations/page.tsx
@@ -0,0 +1,24 @@
+import { Header, Menu, Content, Footer } from "@/components/interface"
+
+export default function Page() {
+    return (
+        <div>
+            <Header
+                title="AirBnB"
+                subtitle="Minhas acomodações"
+            />
+
+            <Menu />
+
+            <Content>
+                <h2>Página de acomodações</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Delectus ut nisi voluptas quisquam officiis aperiam in corrupti commodi cum minus, doloremque, atque laborum porro nam vitae qui ullam deserunt quo?
+                </p>
+            </Content>
+
+            <Footer />
+        </div>
+    )
+}
--- src/components/interface.tsx
+++ src/components/interface.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export function Accommodation() {
     return (
         <div>
@@ -11,11 +13,8 @@ export function Accommodation() {
 export function Header( props: any ) {
     return (
         <div className="bg-teal-600">
-            <h1>Logo</h1>
             <p>{props.title}</p>
-            <p>{props.description}</p>
-            <p>{props.price}</p>
-            {Menu()}
+            <p>{props.subtitle}</p>
         </div>
     )
 }
@@ -23,7 +22,33 @@ export function Header( props: any ) {
 export function Menu() {
     return (
         <div className="bg-teal-600">
-            Acododações | Experiências
+            <MenuItem text="Início" url="/home" />
+            <MenuItem text="Acomodações" url="/accomodations" />
+            <MenuItem text="Help" url="/home" />
+        </div>
+    )
+}
+
+export function MenuItem( props: any ) {
+    return (
+        <Link href={props.url} className="p-2">
+            {props.text}
+        </Link>
+    )
+}
+
+export function Footer() {
+    return (
+        <div className="bg-teal-400">
+            <p>Rodapé</p>
+        </div>
+    )
+}
+
+export function Content( props: any ) {
+    return (
+        <div className="bg-blue-400">
+            {props.children}
         </div>
     )
 }
